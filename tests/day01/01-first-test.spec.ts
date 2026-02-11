ee1798599f6650c78c9ea673f6168e940c5bbdbd
Emre MENTES <emreowen57@gmail.com>
2026-02-11 23:10:23 +0100

    changes

--- tests/day01/01-first-test.spec.ts
+++ tests/day01/01-first-test.spec.ts
@@ -1,4 +1,3 @@
-
 import {test,expect} from "@playwright/test"
 
 /*
@@ -6,15 +5,23 @@ syntax
 test("buraya testimizin basligini yazariz",()=>{
 // testlerimizle ilgili kodlari buraya yazacağız
 });
+
+
+promise : Tamamlanmasi zaman alacak bir islemin sonucunu o sonuc gelene kadar kod icinde temsil eder, ve hatasiz testler yazabilmek icin promise return eden bir methodun basina mutlaka await keywordunu koymaliyiz
+
+await : asenkron bir islemden sonuc gerceklesene kadar kodun bir alt satira devam etmesini duraklatan komuttur
+
+async : bir fonksiyon icinde await(bekleme) yapilabileceğini ve bu fonksiyon icinde promise return eden yapilar bulundugunu belirtir
+ve await kullanabilmek icin fonkisyonu async olarak isaretlememiz gerekir
 */
 
-test("Sayfa Basligini dogrula",({page})=>{
+test("Sayfa Basligini dogrula",async({page})=>{
     
 //Google sayfasina gidelim
-page.goto("https://www.google.com");
+await page.goto("https://www.google.com");
 
 //Title bilgisini alalim
-const title = page.title();
+const title : string = await page.title();
 
 //expect ile title i doğrulayalim
 expect(title).toBe("Google");
