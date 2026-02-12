3835e782cc262fc3c7aa4c0c822fa0691b31c55f
Emre MENTES <emreowen57@gmail.com>
2026-02-12 23:46:06 +0100

    changes

---
+++ tests/day04/02-navigation.spec.ts
@@ -0,0 +1,21 @@
+import { test } from "@playwright/test";
+
+test("Navigation Methods", async ({ page }) => {
+  //   https://www.techproeducation.com/ adresine gidiniz
+  await page.goto("https://www.techproeducation.com/");
+
+  //   https://www.amazon.com/ adresine gidiniz
+  await page.goto("https://www.amazon.com/");
+
+  //   Tarayıcıda bir önceki sayfaya (Techpro) geri dönünüz
+  await page.goBack();
+
+  //   Tekrar ileri sayfaya (Amazon) gidiniz
+  await page.goForward();
+
+  //   Mevcut sayfayı (Amazon) yenileyiniz (F5 yapınız)
+  await page.reload();
+
+  //   sayfayi kapatiniz
+  await page.close();
+});
