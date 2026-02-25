7046002a8a19da51e33735b11a5b86e24cc3158f
Emre MENTES <emreowen57@gmail.com>
2026-02-25 14:59:48 +0100

    changes

---
+++ tests/day11/01-timeouts.spec.ts
@@ -0,0 +1,18 @@
+import { test, expect } from "@playwright/test";
+
+test("test timeout practice", async ({ page }) => {
+  //test fonkisyonunun varsayilan timeout süresi 30 saniyedir
+  test.setTimeout(5000); //bu test fonksiyonu icin timeout süresini 5 saniye olarak güncelledik
+
+  console.log("test timeout kronometremiz basladi...");
+
+  await page.waitForTimeout(6000); //40000 milisecond boyunca hicbirsey yapmadan bu satirda bekle
+  console.log(
+    "BU SATIR ASLA OKUNMAYACAK CUNKU TEST 30 SANİYE SONRA TIMEOUT ERROR VEREREK DURDURULACAK",
+  );
+});
+
+test("action timeout practice", async ({ page }) => {
+  await page.goto("https://the-internet.herokuapp.com/dynamic_loading/1");
+  await page.getByRole("button", { name: "Start" }).click({ timeout: 1000 });
+});
