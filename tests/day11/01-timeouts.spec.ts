13e7186010fd70aecab9a0bce81b671f24fb98d3
Emre MENTES <emreowen57@gmail.com>
2026-02-26 09:45:12 +0100

    changes

--- tests/day11/01-timeouts.spec.ts
+++ tests/day11/01-timeouts.spec.ts
@@ -17,8 +17,27 @@ test("action timeout practice", async ({ page }) => {
   await page.getByRole("button", { name: "Start" }).click({ timeout: 1000 });
 });
 
+test("navigation timeout practice", async ({ page }) => {
+  //google sayfasinin 100 milisaniye icinde acilmasini aksi halde timeout error vermesini sagladik
+  await page.goto("https://google.com", { timeout: 100 });
+});
 
-test('navigation timeout practice', async ({ page }) => {
-    //google sayfasinin 100 milisaniye icinde acilmasini aksi halde timeout error vermesini sagladik
-    await page.goto("https://google.com",{timeout:100});
+test("expect timeout practice", async ({ page }) => {
+
+  await page.goto("https://the-internet.herokuapp.com/dynamic_loading/1");
+
+  await page.getByRole("button", { name: "Start" }).click();
+
+  const helloWorldWebElement = page.getByText("Hello World!");
+
+//expect timeout default olarak 5 saniyedir. bu süre yeterli gelmedigi icin güncelleyebiliriz
+  await expect(helloWorldWebElement).toBeVisible({timeout:10000});
+});
+
+test('test slow practice', async ({ page }) => {
+
+    test.slow();//bu test icin test timeout olarak ayarlanmis olan süreyi 3 ile carpar.
+    await page.waitForTimeout(50000);
+
+    
 });
