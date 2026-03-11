c4e94f3cf498979f952368dd5dc63fba397e4d8f
Emre MENTES <emreowen57@gmail.com>
2026-03-11 06:43:48 +0100

    changes

--- tests/day21/01-reading-data-from-excel.spec.ts
+++ tests/day21/01-reading-data-from-excel.spec.ts
@@ -1,11 +1,22 @@
-import * as xlsx from "xlsx"
+import * as xlsx from "xlsx" 
 
-const workbook= xlsx.readFile("test-data/loginTestData.xlsx");
-const sheetName= workbook.SheetNames[0]
+//Belirttigimiz klasöre gider ve excel dosyasinin tamamini bir bütün olarak kodumuzun icine dahil eder
+const workbook = xlsx.readFile("test-data/loginTestData.xlsx");
 
+//excel dosyasindaki tüm sayfa isimlerine bakar ve index ile sayfa ismi secmemizi saglar, index 0 girdigimiz icin ilk sayfa ismini secmis olduk
+const sheetName = workbook.SheetNames[0];
+
+//köseli parantez icinde belirteceğimiz sayfanın tüm tablo icerigini bize verir
 const sheet = workbook.Sheets[sheetName];
-const testData = xlsx.utils.sheet_to_json(sheet)
 
-console.log(testData)
+const testData : any[] = xlsx.utils.sheet_to_json(sheet);
 
-
+console.log(testData);
+/*
+[
+  { username: 'standard_user', password: 'secret_sauce' },
+  { username: 'problem_user', password: 'secret_sauce' },
+  { username: 'error_user', password: 'secret_sauce' },
+  { username: 'visual_user', password: 'secret_sauce' }
+]
+*/
