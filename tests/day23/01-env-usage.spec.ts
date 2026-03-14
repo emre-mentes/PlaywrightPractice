39bd02112a2887687d7f0dd576705a5633b9dc06
Emre MENTES <emreowen57@gmail.com>
2026-03-14 14:08:19 +0100

    changes

--- tests/day23/01-env-usage.spec.ts
+++ tests/day23/01-env-usage.spec.ts
@@ -1,9 +1,14 @@
 import * as dotenv from "dotenv"
 
-//.env dosyasini okuyup icindeki tum değiskenleri process.env objectine yukluyoruz,
-// artik bu sayede process.env.DEGİSKEN_ADI seklinde değiskenlere erisebiliriz
+//.env dosyasini okuyup icindeki tum değiskenleri process.env objectine yukluyoruz, artik bu sayede process.env.DEGİSKEN_ADI seklinde değiskenlere erisebiliriz
 //process Node.js in yerlesik ve global bir objectidir ve programin calistigi ortam hakkinda bilgileri tutar
 dotenv.config();
 
+const validUserNAME =process.env.SAUCEDEMO_USERNAME_VALID;
+console.log("validUserNAME :",validUserNAME);
 
+const validPassword = process.env.SAUCEDEMO_PASSWORD_VALID;
+console.log("validPassword :", validPassword);
 
+const baseUrl = process.env.BASE_URL;
+console.log("BASE_URL :", baseUrl);
