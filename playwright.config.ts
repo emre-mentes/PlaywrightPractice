90451bd4e3c648e794d0ee193ad31ac65e8c5c37
Emre MENTES <emreowen57@gmail.com>
2026-03-15 21:35:42 +0100

    changes

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,7 +4,9 @@ import { defineConfig, devices } from '@playwright/test';
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// import dotenv from 'dotenv';
+import dotenv from 'dotenv';
+dotenv.config();
+
 // import path from 'path';
 // dotenv.config({ path: path.resolve(__dirname, '.env') });
 
@@ -36,7 +38,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
+    //baseURL: process.env.BASE_URL || "https://www.saucedemo.com/",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     screenshot:"only-on-failure",
@@ -52,6 +54,12 @@ export default defineConfig({
 
   /* Configure projects for major browsers */
   projects: [
+
+    {
+      name: "batch366",
+      use: { ...devices["Desktop Chrome"] },
+    },
+
     {
       name: "chromium",
       use: { ...devices["Desktop Chrome"] },
---
+++ tests/day23/02-env-usage-test.spec.ts
@@ -0,0 +1,44 @@
+import { test, expect } from "@playwright/test";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+
+test("login testi", async ({ page }) => {
+  //   Tarayıcıda https://www.saucedemo.com/ adresine git
+  await page.goto(process.env.BASE_URL || "https://www.saucedemo.com/");
+  // || => soldaki deger herhangi bir falsy(null,undefined,"",0,false) deger ise sagdaki degeri kullan demek
+
+  const usernameFiled = page.getByTestId("username");
+  const passwordFiled = page.getByTestId("password");
+  const loginButton = page.getByTestId("login-button");
+  const productsTitle = page.getByTestId("title");
+
+  //! => typescripte güven bana bu data kesinlikle gelecek demis olduk, eger data gelmezse hata fırlatir
+
+  //   Username alanına "standard_user" yaz
+  await usernameFiled.fill(process.env.SAUCEDEMO_USERNAME_VALID!);
+
+  //?? => nullish coalescing operator()  ?? solundaki deger undefined yada null ise sagdaki degeri kullan demek, sadece null ve undefined ise devreye girer
+  //   Password alanına "secret_sauce" yaz
+  await passwordFiled.fill(
+    process.env.SAUCEDEMO_PASSWORD_VALID ?? "secret_sauce",
+  );
+  //   Login butonuna tıkla
+  await loginButton.click();
+  //   Login isleminin basarili oldugunu dogrula
+  await expect(productsTitle).toBeVisible();
+});
+
+test("dotenv base url kullanimi", async ({ page }) => {
+  /*use: {
+    baseURL: process.env.BASE_URL || "https://www.saucedemo.com/",
+    }
+    config ts dosyamizdaki use icinde baseurli bu sekilde tanimladigimiz zaman bir baseurl değisikligi oldugunda güncellemek cok daha kolay olur, tüm testler buradaki base urli baz alir. await page.goto("/"); diyerek kullanmamiz yeterli olur
+    diger yöntemle hard code olur, bir değisiklik oldugunda herbirisini tek tek güncellemek gerekir
+
+*/
+
+  await page.goto("/");
+
+  // await page.goto("https://www.saucedemo.test.com/");
+});
