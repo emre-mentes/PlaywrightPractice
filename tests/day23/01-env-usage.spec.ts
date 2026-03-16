b0dd46f4805d92d1b82125ddeae64c531f6b1b9f
Emre MENTES <emreowen57@gmail.com>
2026-03-16 18:24:07 +0100

    changes

--- tests/day23/01-env-usage.spec.ts
+++ tests/day23/01-env-usage.spec.ts
@@ -1,6 +1,5 @@
 import * as dotenv from "dotenv"
 
-
 //.env dosyasini okuyup icindeki tum değiskenleri process.env objectine yukluyoruz, artik bu sayede process.env.DEGİSKEN_ADI seklinde değiskenlere erisebiliriz
 //process Node.js in yerlesik ve global bir objectidir ve programin calistigi ortam hakkinda bilgileri tutar
 dotenv.config();
