294a6b9f77a6a89b3cd37f9e326532f666cef312
Emre MENTES <emreowen57@gmail.com>
2026-03-18 09:39:55 +0100

    changes

--- tests/smoke/day25/02-storage-state.spec.ts
+++ tests/smoke/day25/02-storage-state.spec.ts
@@ -1,5 +1,7 @@
 import { test, expect } from "@playwright/test";
 
+
+test.use({ storageState: { cookies: [], origins: [] } });
 test("login testi", async ({ page }) => {
   await page.goto("https://www.saucedemo.com/");
 
