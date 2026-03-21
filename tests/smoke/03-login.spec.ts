65866952331bb9bdef245a649fca430cf809c869
Emre MENTES <emreowen57@gmail.com>
2026-03-21 13:14:31 +0100

    changes

--- tests/smoke/03-login.spec.ts
+++ tests/smoke/03-login.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from "@playwright/test";
 
 
 test("login testi", async ({ page }) => {
-  //   Tarayıcıda https://www.saucedemo.com/ adresine git
+  //  Tarayıcıda https://www.saucedemo.com/ adresine git
   await page.goto("https://www.saucedemo.com/");
 
   const usernameFiled = page.getByTestId("username");
@@ -10,9 +10,9 @@ test("login testi", async ({ page }) => {
   const loginButton = page.getByTestId("login-button");
   const productsTitle = page.getByTestId("title");
 
-  //   Username alanına "standard_user" yaz
+  //  Username alanına "standard_user" yaz
   await usernameFiled.fill("standard_user");
-  //   Password alanına "secret_sauce" yaz
+  //  Password alanına "secret_sauce" yaz
   await passwordFiled.fill("secret_sauce");
   //   Login butonuna tıkla
   await loginButton.click();
